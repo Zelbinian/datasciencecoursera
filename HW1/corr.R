7e1a44dacb78491cc7765084187d9146d961483c
Dustin Hodge <dlhodge@uw.edu>
2015-04-17 10:55:12 -0700

    First attempt to write the corr function

---
+++ HW1/corr.R
@@ -0,0 +1,28 @@
+corr <- function(directory, threshold = 0) {
+    
+    # first thing's first - if the directory doesn't exist, not much that can be done
+    if(!file.exists(directory)) {
+        
+        stop(paste("The provided directory '", directory, "' does not exist."))
+    }
+    
+    # need to store our list of correlations somewhere
+    correlations <- vector()
+    
+    # we're still here, let's iteratively examine each csv file in the provided dir
+    for(csv in list.files(path = directory, pattern = "*.csv")) {
+        
+        file <- read.csv(csv)
+        
+        # if threshold is zero, not much to do
+        if (threshold == 0) {
+            
+            correlations <- c(correlations, cor(file["nitrate"], file["sulfate"]))
+            
+        } else if (nrow(na.omit(file)) > threshold) {
+            
+            correlations <- c(correlations, cor(file["nitrate"], file["sulfate"]))
+            
+        }
+    }
+}
