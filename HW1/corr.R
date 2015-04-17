f01cbcf2528903b6cec41f52ce15dc9d8f83977a
Dustin Hodge <dlhodge@uw.edu>
2015-04-17 12:32:09 -0700

    Removing na rows before calculating correlation

--- HW1/corr.R
+++ HW1/corr.R
@@ -10,12 +10,13 @@ corr <- function(directory, threshold = 0) {
     correlations <- vector()
     
     # we're still here, let's iteratively examine each csv file in the provided dir
-    for(csv in list.files(path = directory, pattern = "*.csv"m full.names = TRUE)) {
+    for(csv in list.files(path = directory, pattern = "*.csv", full.names = TRUE)) {
         
-        file <- read.csv(csv)
+        # reading the csv and omitting na rows in one fell swoop
+        file <- na.omit(read.csv(csv)) 
         
-        # if threshold is zero, not much to do
-        if (nrow(na.omit(file)) > threshold) {
+        # only do the correlation thing on this file if rows > threshold
+        if(nrow(file) > threshold) {
             
             correlations <- c(correlations, cor(file["nitrate"], file["sulfate"]))
             
