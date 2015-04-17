a75415356e5ba7a2cd3eeed3d11f2d6cd16fc918
Dustin Hodge <dlhodge@uw.edu>
2015-04-17 10:00:10 -0700

    First pass at creating the complete function
    Reused a lot of code from the other function

---
+++ HW1/complete.R
@@ -0,0 +1,36 @@
+complete <- function(directory, id = 1:332) {
+    
+    # first thing's first - if the directory doesn't exist, not much that can be done
+    if(!file.exists(directory)) {
+        
+        stop(paste("The provided directory '", directory, "' does not exist."))
+    }
+    
+    # instantiating empty vectors to add to later
+    stationIds <- vector()
+    numObs <- vector()
+    
+    # we're still here? Great, let's loop!
+    for (station in id) {
+        
+        # building the file name separately for better code readability
+        file <- paste0(directory, '/', formatC(station, width = 3, flag = 0), '.csv')
+        
+        stationData <- read.csv(file)
+        
+        # I could do things with the id number before all this, but if the file doesn't
+        # exist, we want to let read.csv throw the error. Best practice would be to
+        # try-catch it, but I'm not sure how error handling works in R right now
+        
+        stationIds <- c(stationIds, station)
+        numObs <- c(numObs,                        # summing the actual obs
+                sum(!is.na(stationData["sulfate"])) + 
+                sum(!is.na(stationData["nitrate"]))
+        )
+    }
+    
+    # assembling for output
+    output <- data.frame(stationIds, numbObs)
+    names(output) <- c("id", "nobs")
+    output  
+}
