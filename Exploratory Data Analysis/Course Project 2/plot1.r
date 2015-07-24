29ce685f5090299451e94c91778393dac85e29c8
Dustin Hodge <dlhodge@uw.edu>
2015-07-24 14:52:41 -0700

    Removing unneeded file
    First plot doesn't need both files

--- Exploratory Data Analysis/Course Project 2/plot1.r
+++ Exploratory Data Analysis/Course Project 2/plot1.r
@@ -1,8 +1,8 @@
-# Yo, you got my files?
-fileRegEx = "Source_Classification_Code.rds|summarySCC_PM25.rds"
+# yo, you got my files?
+fileRegEx = "summarySCC_PM25.rds"
 
 if(length(dir(pattern = fileRegEx)) != 2) {
-    # no? laaazyyyyyyy. I'll download 'em for you.
+    # no? laaazyyyyyyy. i'll download 'em for you
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
                   "plotData.zip")
     unzip("plotData.zip")
@@ -11,4 +11,3 @@ if(length(dir(pattern = fileRegEx)) != 2) {
 
 # k, let's take a look
 NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
