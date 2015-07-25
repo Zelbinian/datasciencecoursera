bdbec7c261716aa51d12cd11873e3d7750e5dcec
Zelbinian <dustin.l.hodge@gmail.com>
2015-07-24 17:22:27 -0700

    Bug in file checker

--- Exploratory Data Analysis/Course Project 2/plot2.r
+++ Exploratory Data Analysis/Course Project 2/plot2.r
@@ -1,7 +1,7 @@
 # yo, you got my files?
 fileRegEx = "summarySCC_PM25.rds"
 
-if(length(dir(pattern = fileRegEx)) != 2) {
+if(length(dir(pattern = fileRegEx)) != 1) {
     # no? laaazyyyyyyy. i'll download 'em for you
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
                   "plotData.zip")
