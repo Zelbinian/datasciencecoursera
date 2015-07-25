869e8f852d5cb2ecc95d41439d06ec588d4dd744
Zelbinian <dustin.l.hodge@gmail.com>
2015-07-24 17:21:38 -0700

    Printing to PNG
    Also fixed a bug in the file checker

--- Exploratory Data Analysis/Course Project 2/plot1.r
+++ Exploratory Data Analysis/Course Project 2/plot1.r
@@ -1,7 +1,7 @@
 # yo, you got my files?
 fileRegEx = "summarySCC_PM25.rds"
 
-if(length(dir(pattern = fileRegEx)) != 2) {
+if(length(dir(pattern = fileRegEx)) != 1) {
     # no? laaazyyyyyyy. i'll download 'em for you
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
                   "plotData.zip")
@@ -16,9 +16,10 @@ NEI <- readRDS("summarySCC_PM25.rds")
 pm25sums <- with(NEI, tapply(Emissions, as.factor(year), sum))
 
 # like my art skills?
+png("plot1.png")
 plot(names(pm25sums), pm25sums, type = "b", col = "blue", xlab = "Year", xaxt = "n",
      yaxt = "n", ylim = c(3000000,8000000), ylab = "Total PM2.5 Emissions (millions)",
      main = "Trendline of PM2.5 Emissions in the United States")
 axis(1, names(pm25sums))
 axis(2, seq(3000000,8000000,500000), label = seq(3,8,.5))
-
+dev.off()
